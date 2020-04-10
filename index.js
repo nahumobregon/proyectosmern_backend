e06ab2760fc1169810032ed8de674d5d25543949
nahumobregon <nahumobregon@gmail.com>
2020-04-10 00:58:10 -0500

    modificando index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(express.json({extended: true }))
 
 //puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //Importar Rutas
 app.use('/api/usuarios',require('./routes/usuarios'))
@@ -30,6 +30,6 @@ app.get('/',(req,res)=>{
 })
 
 //arrancar el servidor
-app.listen(PORT, ()=>{
+app.listen(port,'0.0.0.0', ()=>{
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
